cde0af645d8dfb69c373ee391039fc0cf82bb672
Youssef-Wheed <wayoussef138@gmail.com>
2026-02-27 15:22:43 +0200

    Fix discount calculation logic in Quiz_2.dart

--- Task 1/Quiz_2.dart
+++ Task 1/Quiz_2.dart
@@ -1,9 +1,15 @@
 
+void main() {
+  print(discount(OriginalPrice: 100.0, isPremium: true, hasCoupon: false)); // Output: 85.0
+  print(discount(OriginalPrice: 100.0, isPremium: false, hasCoupon: true)); // Output: 85.0
+  print(discount(OriginalPrice: 100.0, isPremium: false, hasCoupon: false)); // Output: 100.0
+}
+
 double discount ({
   required double OriginalPrice,
   required bool isPremium,
   required bool hasCoupon,
 })
 {
-  return (isPremium || hasCoupon) ? (OriginalPrice * 0.15) : OriginalPrice;
+  return (isPremium || hasCoupon) ? (OriginalPrice * 0.85) : OriginalPrice;
 }
