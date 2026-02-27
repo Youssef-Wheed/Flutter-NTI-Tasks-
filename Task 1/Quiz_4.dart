c89a6cd3be0c9c66a7e15df28d01ab45c9c11728
Youssef-Wheed <wayoussef138@gmail.com>
2026-02-27 16:25:11 +0200

    Fix checkLoan function to return "Loan Rejected" instead of "Loan Denied"

--- Task 1/Quiz_4.dart
+++ Task 1/Quiz_4.dart
@@ -15,6 +15,6 @@ String checkLoan(
   if (salary >= 5000 && age >= 21 && age <= 60 && !hasExistingLoan) {
     return "Loan Approved";
   } else {
-    return "Loan Denied";
+    return "Loan Rejected";
   }
 }
