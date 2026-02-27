756e63a2414589596f44cdebecbe9352c3ea54dd
Youssef-Wheed <wayoussef138@gmail.com>
2026-02-27 15:18:43 +0200

    add condition of discount function

--- Task 1/Quiz_2.dart
+++ Task 1/Quiz_2.dart
@@ -1,11 +1,9 @@
 
-int discount ({
+double discount ({
   required double OriginalPrice,
   required bool isPremium,
   required bool hasCoupon,
 })
 {
-  
-
-  }
+  return (isPremium || hasCoupon) ? (OriginalPrice * 0.15) : OriginalPrice;
 }
