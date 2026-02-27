a1245e1daf7b71307cd9dfea22d86d755f339608
Youssef-Wheed <wayoussef138@gmail.com>
2026-02-27 15:52:34 +0200

    Add main function to demonstrate totalPayment calculations with sample inputs

--- Task 1/Quiz_5.dart
+++ Task 1/Quiz_5.dart
@@ -1,7 +1,11 @@
+void main(){
+  print(totalPayment(order_amount: 250, distance: 10)); // Output: 300.0
+  print(totalPayment(order_amount: 350, distance: 10)); // Output: 350.0
+}
+
 double totalPayment({required double order_amount, required double distance}) {
   if (order_amount >= 300)
     return order_amount;
   else 
     return order_amount + (distance * 5);
-  
 }
