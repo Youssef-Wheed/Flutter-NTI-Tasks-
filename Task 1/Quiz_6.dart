26a2dbf583c8618f0a55cbf00afa9e51bc979265
Youssef-Wheed <wayoussef138@gmail.com>
2026-02-27 15:58:40 +0200

    Add main function to demonstrate evaluateEmployeeBonus functionality with sample inputs

--- Task 1/Quiz_6.dart
+++ Task 1/Quiz_6.dart
@@ -1,3 +1,8 @@
+void main(){
+  print(evaluateEmployeeBonus(performanceRating: 5, experience: 4)); // Output: Bonus Granted
+  print(evaluateEmployeeBonus(performanceRating: 3, experience: 2)); // Output: No Bonus
+}
+
 String evaluateEmployeeBonus({
   required int performanceRating,
   required int experience,
