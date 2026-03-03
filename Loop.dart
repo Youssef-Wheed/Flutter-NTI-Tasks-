57e28278ce81ae35adb362f3e414b1044c49818f
Youssef-Wheed <wayoussef138@gmail.com>
2026-03-03 12:24:28 +0200

    Implement printNumbers method to output numbers from 1 to 10

--- Loop.dart
+++ Loop.dart
@@ -1,3 +1,10 @@
-class  Loop {
+class Loop {
+  // This method prints numbers from 1 to 10 using a for loop
+  void printNumbers() {
+    for (int i = 1; i <= 10; i++) {
+      print(i);
+    }
+  }
+
   
 }
